6d730abc4bfc6a27d4a2b332678d74d63e33a6f0
Sean Mack <sjm3758@rit.edu>
2018-04-06 09:58:58 -0400

    Created a manager singleton script to hold player stats and other game information

---
+++ Assets/Scripts/ManagerSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerSingleton : MonoBehaviour {
+
+    private static ManagerSingleton _instance;
+
+    public static ManagerSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<ManagerSingleton>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
