f22198d8e415a27eb63d7ea94543a24f559da801
Vincent Lamicela <vtl9301@rit.edu>
2018-04-11 10:00:52 -0400

    Tinkering with code a little.

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+    float speed;
+    Vector2 forward;
+    Vector2 maxPos = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+	// Use this for initialization
+	void Start () {
+        speed = 8.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 position = transform.position;
+        position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+        transform.position = position;
+
+        if(transform.position.y > maxPos.y)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,10 +7,14 @@ public class PlayerScript : MonoBehaviour {
 
     private Vector3 startingPosition;
     private Vector3 pos;
+    private float rot;
 
+    private Vector2 forward; //keeps track of direction it's pointing
     public float speed = 5.0f;
     private float deltaX;
     public float speedCap = 15.0f;
+
+    private Rigidbody2D m_RigidBody;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +30,7 @@ public class PlayerScript : MonoBehaviour {
 
     void Move()
     {
+        m_RigidBody = gameObject.GetComponent<Rigidbody2D>();
         //check for move input, add to velocity appropriately
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
@@ -34,6 +39,9 @@ public class PlayerScript : MonoBehaviour {
 
         this.gameObject.GetComponent<Transform>().position = pos;
         this.gameObject.GetComponent<Rigidbody2D>().velocity = velocity;
+        //rotate based on moveX
+        //rot = moveX * speed * Time.deltaTime;
+        //Quaternion turnQuat = Quaternion.Euler(0f, 0f, rot);
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * speed, moveY * speed);
     }
 }
