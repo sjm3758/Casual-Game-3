3dcb170ed6f748054ecc50a79780ac9bad7f908d
Plat <maverick.alpha4@gmail.com>
2018-04-12 23:30:45 -0400

    Added dumb enemy and following enemy

--- Assets/Scripts/DumbEnemy.cs
+++ Assets/Scripts/DumbEnemy.cs
@@ -8,11 +8,15 @@ public class DumbEnemy : MonoBehaviour {
     public Transform target;
     public float range;
     private float distance;
+    private Quaternion q;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         speed = 3f;
-	}
+        Vector3 targetDir = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+        q = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,9 +25,10 @@ public class DumbEnemy : MonoBehaviour {
         distance = Vector3.Distance(transform.position, target.position);
 
             //Chase player - turn
-            Vector3 targetDir = target.position - transform.position;
+           /* Vector3 targetDir = target.position - transform.position;
             float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
-            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            q = Quaternion.AngleAxis(angle, Vector3.forward);
+            */
             transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
             transform.Translate(Vector3.up * Time.deltaTime * speed);
 
